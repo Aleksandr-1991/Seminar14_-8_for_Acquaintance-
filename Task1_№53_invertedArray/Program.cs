06c3c5c80ddab4b2b4bd7ed8b62726a76e5e6a11
Aleks37 <lao35180972643335589007>
2024-04-12 21:22:00 +0400

    Add these Tasks, what were remoted, again. From 12.04.24

---
+++ Task1_№53_invertedArray/Program.cs
@@ -0,0 +1,53 @@
+﻿// №53. Прога в 2d-массиве мен местами 1 и последнюю строки массива.
+
+int Promt (string text){
+    Console.WriteLine(text);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int [,] GetMatrix()
+{
+    int m = Promt("Введите значение m, отвечающее за кол-во строк: ");
+    int n = Promt("Введите значение n, отвечающее за кол-во столбцов: ");
+    int k = 0;
+    int [,]  array2d = new int [m, n];
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){  
+            // array2d [i, j] = new Random().Next(1, 10);
+            array2d [i, j] = k;
+            k++;
+        }
+    }
+    return array2d;
+}
+
+void InvertedMatrix(int [,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++){
+        int temp = matrix [0, j];
+        matrix [0, j] = matrix [matrix.GetLength(0) - 1, j];
+        matrix [matrix.GetLength(0) - 1, j] = temp;
+    }
+}
+
+void PrintMatrix (int [,]  matrix){
+    Console.Write("[");
+    for (int i = 0; i < matrix.GetLength(0); i++){
+        if (i == matrix.GetLength(0) - 1){
+            for (int j = 0; j < matrix.GetLength(1); j++){
+                if (j == matrix.GetLength(1) - 1)  Console.Write($"{matrix[i, j]:f1}");
+                else Console.Write ($"{matrix[i, j]:f1}\t");
+            }
+            Console.WriteLine("]");
+        }
+        else{
+            for (int j = 0; j < matrix.GetLength(1); j++) Console.Write($"{matrix[i, j]:f1}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] matrix = GetMatrix();
+PrintMatrix(matrix);
+InvertedMatrix(matrix);
+PrintMatrix(matrix);
---
+++ Task2_№666_MidleFromAngles/Program.cs
@@ -0,0 +1,44 @@
+﻿// №666. Прога считает среднее-арифм меж эл-тами в углах 2d-массива.
+
+int Promt (string text){
+    Console.WriteLine(text);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[,] GenerateRandomMatrix()
+{
+    int m = Promt("Введите значение m, отвечающее за кол-во строк: ");
+    int n = Promt("Введите значение n, отвечающее за кол-во столбцов: ");
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix (int [,]  matrix){
+    Console.Write("[");
+    for (int i = 0; i < matrix.GetLength(0); i++){
+        if (i == matrix.GetLength(0) - 1){
+            for (int j = 0; j < matrix.GetLength(1); j++){
+                if (j == matrix.GetLength(1) - 1)  Console.Write($"{matrix[i, j]:f1}");
+                else Console.Write ($"{matrix[i, j]:f1}\t");
+            }
+            Console.WriteLine("]");
+        }
+        else{
+            for (int j = 0; j < matrix.GetLength(1); j++) Console.Write($"{matrix[i, j]:f1}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = GenerateRandomMatrix();
+PrintMatrix(array);
+Console.Write("Среднее арифметическое меж эл-тами в углах массива равно: ");
+Console.Write((array[0, 0] 
++ array[array.GetLength(0) - 1, 0] 
++ array[array.GetLength(0) - 1, array.GetLength(1) - 1] 
++ array[0, array.GetLength(1) - 1])/4.0);
